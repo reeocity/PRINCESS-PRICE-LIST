98195c653c49f413dd1b3f69b9b235ac296b508d
reeocity <ibraheemreedwan100@gmail.com>
2025-06-14 15:23:04 +0100

    fix: Remove unnecessary client/build static serving for Vercel deployment

--- server.js
+++ server.js
@@ -38,12 +38,14 @@ app.use('/api/outlets', require('./routes/outlets'));
 app.use('/api/users', require('./routes/users'));
 
 // Serve static assets in production
+/*
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+*/
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
